54237bd2cb8caf36394d0bcff475a230b0ab3b06
MARNAT Lorenzo <ml04444r@etumet105>
2017-11-24 09:42:42 +0100

    module personnage

---
+++ src/personnage.h
@@ -0,0 +1,17 @@
+#include <MLV/MLV_all.h>
+
+#define VIE 100
+#define DEGAT 10
+
+typedef struct
+{
+    int x;
+    int y
+} coordonnees;
+
+//Crée un personnage à une position par défaut
+coordonnees perso_creerPerso();
+
+
+//Prend les coordonnes initiales et renvoi les nouvelles
+coordonnees perso_deplacer(map m, coordonnees c, int direction);
